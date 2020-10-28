4a792874a7b74763688d168e125a819fc309b215
Malin Wedin Bäckman <malinwedinbackman@MacBook-Pro-3.lan>
2020-10-28 09:56:25 +0100

    new text js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
-
+// Startar genom att trycka på start 
 function startGame() {
     const whatsYourNamne = prompt('Hej, vad heter du?')
     alert('Hej' + ' ' + whatsYourNamne + ',' + ' ' + 'häng med på en tur i Göteborg!')
-    console.log(whatsYourNamne)
+    //console.log(whatsYourNamne)
 }
 
+// Välj hur du vill ta dig till Domkyrkan
 function firstTransport() {
     const goToFirstLocation = prompt('Vill du åka spårvagn eller promenera?')
     if (goToFirstLocation == 'spårvagn') {
@@ -14,9 +15,9 @@ function firstTransport() {
     else {
         alert('Promenera mot Domkyrkan. Men se upp, här i Göteborg har spårvagnarna alltid företräde.')
     }
-    console.log(goToFirstLocation)
+    //console.log(goToFirstLocation)
 }
-
+// Vad tycker du om Domkyrkan
 function whatDoYouThinkofDomkyrkan() {
     const theLookofDomkyrkan = prompt('Nu är du framme vid Domkyrkan, vad tycker du? Är den fin eller ful?')
     if (theLookofDomkyrkan == 'ful') {
@@ -27,6 +28,7 @@ function whatDoYouThinkofDomkyrkan() {
     }
 }
 
+// Fråga om kaffe
 function inTheMoodForACupOfCoffee() {
     const drinkCoffee = prompt('Gillar du kaffe?')
     if (drinkCoffee == 'ja') {
@@ -36,11 +38,12 @@ function inTheMoodForACupOfCoffee() {
         alert('Gå till da Matteo på vallgatan ändå och ät en god bulle!')
     }
 }
-
+// På da Matteo
 function atDaMatteo() {
     alert('Du är nu framme på da Matteo och du ska beställa i kassan men inser att din plånbok inte längre ligger i din ficka.')
 }
 
+// I kassan på da Matteo
 function atCashierDaMatteo() {
     const missingWallet = prompt('Vad gör du nu, suckar uppgivet  eller skriker i förtvivlan: min plånbok är borta?')
     if (missingWallet == 'suckar uppgivet') {
@@ -48,8 +51,11 @@ function atCashierDaMatteo() {
         const doYouTurnAround = prompt('Vänder du dig om för att se vem som knackar dig på axeln?')
         if (doYouTurnAround == 'ja')
         alert('Du vänder dig om och ser Håkan Hellström som säger: Klart du ska ha fika, jag bjuder!')
+        alert('Du tar din kopp kaffe och sätter dig vid en bänk mot väggen och njuter av solen.')
+        return
     }    
     if (missingWallet == 'skriker i förtvivlan') {
         alert('Baristan ber dig gå åt sidan medan du letar efter din plånbok')
+        alert('Du lämnar da Matteo och möts av ett grått och regnigt Göteborg.')
 }    
 }
