4b92b7950d4aa7e7f810229cf43fe3fca65fbf3e
Malin Wedin Bäckman <malinwedinbackman@MacBook-Pro-3.lan>
2020-10-30 13:17:53 +0100

    uppdated functions

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 En textbaserad interavktiv upplevelse 
 
+Titel: Solig dag i Göteborg
+
+Beskrivning:
+En solig dag Göteborg, hänger du med så får du se ett av de bästa sätten att spendera just en solig dag i centrala delarna av Göteborg, med något gott i handen. 
+
+
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 // Startar genom att trycka på start-knappen 
 function startGame() {
     const whatsYourNamne = prompt('Hej, vad heter du?')
-    alert('Hej' + ' ' + whatsYourNamne + ',' + ' ' + 'häng med!')
-    
+    alert('Hej' + ' ' + whatsYourNamne + ',' + ' ' + 'häng med!')  
+    firstTransport()
 }
 
 // Välj hur du vill ta dig till Domkyrkan
@@ -11,25 +10,26 @@ function firstTransport() {
     const goToFirstLocation = prompt('Du ska ta dig till Domkyrkan. Vill du åka spårvagn eller promenera?')
     if (goToFirstLocation == 'spårvagn') {
         alert('Åk spårvagn nr 11 till Domkyrkan')
-    }
-    else if (goToFirstLocation == 'promenera') {
-        alert('Promenera mot Domkyrkan. Men se upp, här i Göteborg har spårvagnarna alltid företräde.')
-    }
-    else {
+        whatDoYouThinkofDomkyrkan()
+    } else if (goToFirstLocation == 'promenera') {
+        alert('En promenad mot Domkyrkan, härligt i detta vädret. Men se upp, spårvagnarna har alltid företräde.')
+        whatDoYouThinkofDomkyrkan()
+    } else {
         alert('Här blev det fel. Välj "spårvagn" eller "promenera"')
         firstTransport()
     }   
-}
+} 
+
 // Vad tycker du om Domkyrkan
 function whatDoYouThinkofDomkyrkan() {
     const theLookofDomkyrkan = prompt('Nu är du framme vid Domkyrkan, vad tycker du, är den fin eller ful?')
     if (theLookofDomkyrkan == 'ful') {
         alert('Va! Dålig smak!')
-    }
-    else if (theLookofDomkyrkan == 'fin') {
+        inTheMoodForACupOfCoffee()
+    } else if (theLookofDomkyrkan == 'fin') {
         alert('Ja, visst är den fin!')
-    }
-    else {
+        inTheMoodForACupOfCoffee()
+    } else {
         alert('Här blev det fel. Välj "ful" eller "fin"')
         whatDoYouThinkofDomkyrkan()
     }
@@ -40,40 +40,41 @@ function inTheMoodForACupOfCoffee() {
     const drinkCoffee = prompt('Gillar du kaffe?')
     if (drinkCoffee == 'ja') {
         alert('Då rekomenderar jag starkt att du går till da Matteo på Vallgatan, gott kaffe!')
-    }
-    else if (drinkCoffee == 'nej') {
+        atDaMatteo()
+    } else if (drinkCoffee == 'nej') {
         alert('Då kan du gå till da Matteo på Vallgatan och äta en god bulle!')
-    }
-    else {
+        atDaMatteo()
+    } else {
         alert('Här blev det fel. Välj "ja" eller "nej"')
         inTheMoodForACupOfCoffee()
     }
 }
+
 // På da Matteo
 function atDaMatteo() {
     alert('Du är nu framme på da Matteo.')
+    atCashierDaMatteo()
 }
-
 
 // Beställa i kassan
 function atCashierDaMatteo() {
     const canIHaveYourOrder = prompt('Hej! Vad får det lov att vara?')
-        if (canIHaveYourOrder == 'kaffe') {
-            alert('En kaffe till dig!')
-        }
-        else if (canIHaveYourOrder == 'bulle') {
-            alert('En bulle till dig!')
-        }
-        else if (canIHaveYourOrder == 'kaffe och bulle') {
-            alert('Bra val, här har du ditt kaffe och din bulle!')
-        }    
-        else {
-            alert('Här blev det fel. Välj "kaffe" eller "bulle"')
-            atCashierDaMatteo()
-        }        
+    if (canIHaveYourOrder == 'kaffe') {
+        alert('En kaffe till dig!')
+        outdoorSeating()
+    } else if (canIHaveYourOrder == 'bulle') {
+        alert('En bulle till dig!')
+        outdoorSeating()
+    } else if (canIHaveYourOrder == 'kaffe och bulle') {
+        alert('Bra val, här har du ditt kaffe och din bulle!')
+        outdoorSeating()
+    } else {
+        alert('Här blev det fel. Välj "kaffe" eller "bulle"')
+        atCashierDaMatteo()
+    }        
 }
-
+// Till uteserveringen
 function outdoorSeating() {
-    alert('Nu får du sätta dig på den soliga uteserveringen och njuta, hoppas vi ses igen! Hej då!')
+    alert('Nu får du sätta dig på den soliga uteserveringen och njuta av värmen, hoppas vi ses igen! Hej då!')
 }
 
